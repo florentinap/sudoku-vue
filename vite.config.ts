dada3b4009a8b1465ddc6543dbdf4f27ef0fc397
Florentina Petcu <florentinap@4psa.me>
2025-01-27 12:12:11 +0000

    run server on ip instead of local host; fixed UI issues; added more examples for medium difficulty

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ip from 'ip'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? '/sudoku-vue/' : '/',
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    host: ip.address(),
+    port: 3000
+  }
 })
